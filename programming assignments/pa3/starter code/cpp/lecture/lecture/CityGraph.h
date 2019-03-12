1734d97d1a454a13cb305388ab2e5a93eb4894d3
Alex Childers <abc66@humboldt.edu>
2019-03-11 23:40:34 -0700

    Rollback to Tier 2 in progress.

--- programming assignments/pa3/starter code/cpp/lecture/lecture/CityGraph.h
+++ programming assignments/pa3/starter code/cpp/lecture/lecture/CityGraph.h
@@ -4,6 +4,7 @@
 
 #include "StringGraphNode.h"
 #include "Edge.h"
+#include "Path.h"
 #include <unordered_map>
 #include <string>
 #include <queue>
@@ -18,10 +19,21 @@ public:
    }
 };
 
+class TupleComparer
+{
+public:
+	bool operator()(tuple<StringGraphNode*, int, vector<string>> first, tuple<StringGraphNode*, int, vector<string>> second)
+	{
+		return get<1>(first) > get<1>(second); 
+	}
+};
+
 class CityGraph
 {
 private:
 	unordered_map<string, StringGraphNode*> _graph;
+
+	unordered_map<string, unordered_map<string, int>> _shortest_paths{}; 
 
 public:
 	void addVertex(const string& key)
@@ -143,6 +155,7 @@ public:
 				}
 			}
 		}
+		_shortest_paths[start] = distances; 
 		return distances;
 	}
 
@@ -157,6 +170,82 @@ public:
 		{
 			return true;
 		}
+	}
+
+	// returns a HT of shortest paths to other nodes from start
+	unordered_map<string, int> getShortestPaths(const string& start)
+	{
+		// if the shortest distance HT has already been calculated, return it
+		if (_shortest_paths.find(start) != _shortest_paths.end())
+		{
+			return _shortest_paths[start]; 
+		}
+		// else, calculate it and then return it
+		else
+		{
+			return computeShortestPath(start); 
+		}
+	}
+
+	// computes paths from start onward that touch every single node in the graph
+	// returns a hashtable of all paths found that meet this criteria 
+	// (altered form of Dijkstra's algorithm/Prim's MST algorithm)
+	unordered_map<string, Path<string>> findFullPath(const string& start)
+	{
+		// will return this
+		unordered_map<string, Path<string>> paths{}; 
+
+		// keeps track of how many times we've visited a node
+		unordered_map<string, int> visits{}; 
+
+		// make sure we received a valid starting point 
+		if (_graph.find(start) != _graph.end())
+		{
+			priority_queue<tuple<StringGraphNode*, int, vector<string>>,
+				vector<tuple<StringGraphNode*, int, vector<string>>>,
+				TupleComparer> to_visit{}; 
+
+			// prime the PQ with starting location
+			to_visit.push(make_tuple(_graph[start], 0, vector<string>{})); 
+
+			while (to_visit.empty() == false)
+			{
+				//get item on top of PQ
+				auto top = to_visit.top();
+				string key = get<0>(top)->getKey();
+				int weight = get<1>(top);
+				vector<string> path = get<2>(top);
+				to_visit.pop();
+
+				// have we already seen this node twice? 
+				if (visits[key] < 2)
+				{
+					// if not, insert this node at the end of its path 
+					path.push_back(key); 
+
+					// mark as visited and update paths
+					visits[key]++; 
+					paths[key].setWeight(weight); 
+					paths[key].setPath(path);
+
+					// push ALL outgoing edges into PQ
+					for (auto edge: get<0>(top)->getEdges())
+					{
+						// need to dynamically cast from generic graph node
+						// into string graph nodes
+						StringGraphNode* node = dynamic_cast<StringGraphNode*>(edge.first); 
+
+						// has this node already been visited twice?
+						if (visits[key] < 2)
+						{
+							// if not, push it in, accumulating distance and path
+							to_visit.push(make_tuple(node, weight + edge.second, path)); 
+						}
+					}
+				}
+			}
+		}
+		return paths; 
 	}
 };
 
