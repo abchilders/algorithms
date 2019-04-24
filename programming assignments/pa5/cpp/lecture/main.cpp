85225f13a4f6c1ba00fc9f828b06aed20231e033
Alex Childers <abc66@humboldt.edu>
2019-04-24 10:56:34 -0700

    Finished coding a rough draft of PA #5.

--- programming assignments/pa5/cpp/lecture/main.cpp
+++ programming assignments/pa5/cpp/lecture/main.cpp
@@ -156,21 +156,6 @@ int main(void)
 	}
 	words.close();
 
-	// prompt the user for basic file store or load/store in database
-	string storage_method = ""; 
-	cout << "Would you like to store autocorrect results to a file or to "
-		<< "a database? " << endl; 
-	cout << "Enter 1 for file, or 2 for database" << endl; 
-	getline(cin, storage_method);
-
-	// for now, we'll just default to file storage until DB functionality
-	// is implemented
-	if (storage_method == "2")
-	{
-		cout << "Database functionality not available yet. Will store "
-			<< "autocorrect results to a file." << endl; 
-	}
-
 	// prompt the user for an input file to autocorrect and a destination 
 	// to output the corrected text
 	string input_file = ""; 
@@ -215,7 +200,8 @@ int main(void)
 
 					// word is misspelled. do we already have a list of 
 					// autocorrects for it?
-					ifstream autocorrects{ next_word + FILE_SUFFIX }; 
+					string corrections_file = next_word + FILE_SUFFIX; 
+					ifstream autocorrects{ corrections_file }; 
 					if (autocorrects.is_open() == true)
 					{
 						while (autocorrects.good() == true)
@@ -254,12 +240,30 @@ int main(void)
 						}
 
 						// put 10 words with lowest edit distances into vector
-						for (int i = 0; i < 10; i++)
+						int counter = 0; 
+						while (counter < 10)
+						{
+							pair<string, int> next_lowest = edit_distances.top(); 
+
+							// does this word already exist in vector?
+							auto found = find(corrected_words.begin(), 
+											  corrected_words.end(), 
+								              next_lowest);
+							if (found == corrected_words.end())
+							{
+								// if not, add it 
+								corrected_words.push_back(next_lowest);
+								counter++; 
+							}
+							edit_distances.pop(); 
+						}
+						/*for (int i = 0; i < 10; i++)
 						{
 							corrected_words.push_back(edit_distances.top());
 							edit_distances.pop(); 
-						}
+						}*/
 					}
+					autocorrects.close(); 
 
 					// display the 10 most probable answers. let the user pick the 
 					// correct one or specify their own 
@@ -272,6 +276,7 @@ int main(void)
 					{
 						string word = word_pair.first; 
 						cout << numbering << ". " << word << endl; 
+						numbering++; 
 					}
 
 					int choice = 0;
@@ -285,17 +290,40 @@ int main(void)
 						cin >> choice; 
 					}
 
+					string right_word = ""; 
 					if (choice == 1)
 					{
-						// TO DO: record user-defined word
+						// record user-defined word 
+						cout << "Enter corrected word: "; 
+						getline(cin, right_word); 
+						pair<string, int> new_word = make_pair(right_word, 0); 
+						corrected_words.insert(corrected_words.begin(), new_word); 
 					}
 					else
 					{
 						pair<string, int> best_word = corrected_words[choice - 2]; 
+						right_word = best_word.first; 
+						
+						// OUT OF RANGE ERROR HERE- CONTINUE HERE
 						corrected_words.erase(corrected_words.begin() + (choice - 3));
 						corrected_words.insert(corrected_words.begin(), best_word); 
 					}
-				} 
+
+					// output corrected word to file 
+					out_stream << punct.first << right_word << punct.second << " ";
+
+					// output list of autocorrects to file 
+					ofstream new_autocorrects{ corrections_file }; 
+					if (new_autocorrects.is_open() == true)
+					{
+						for (auto word_pair : corrected_words)
+						{
+							new_autocorrects << word_pair.first << " "
+								<< word_pair.second << endl; 
+						}
+					}
+					new_autocorrects.close(); 
+				}
 			}
 		}
 	}
