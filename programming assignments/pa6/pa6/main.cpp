736ac8415fa425a16a61d41b24b2ddec4b6a6ae7
Alex Childers <abc66@humboldt.edu>
2019-05-06 11:46:27 -0700

    Started PA #6.

---
+++ programming assignments/pa6/pa6/main.cpp
@@ -0,0 +1,54 @@
+/*
+Assignment: PA #6 - Hamming Codes 
+Description: This program encodes and decodes Hamming Codes from files given by
+	the user. 
+Author: Alex Childers
+HSU ID: 013145324
+Completion Time: 
+In completing this program, I received help from the following people:
+	N/A
+*/
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include "StringSplitter.h"
+using namespace std;
+
+void encodeFile(string file_name)
+{
+}
+
+void decodeFile(string file_name)
+{
+}
+
+int main(int argc, char* argv[])
+{
+   if (argc != 3)
+   {
+      cout << "Expected format: PA6.exe <encode / decode> <file_name>" << endl;
+      return 0;
+   }
+
+   if (string{ argv[1] } == "encode")
+   {
+      //encode ABC.txt to ABC.txt.coded
+      encodeFile(string{ argv[2] });
+
+      cout << "File encoded." << endl;
+   }
+   else if (string{ argv[1] } == "decode")
+   {
+      //decode ABC.txt.coded to ABC.decoded.txt
+      decodeFile(string{ argv[2] });
+      cout << "File decoded." << endl;
+   }
+   else
+   {
+      cout << "Unexpected command." << endl;
+   }
+
+}
